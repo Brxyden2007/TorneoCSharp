7929cdcd35d44d4f6c49d52e7b80421b55665c72
Brxyden2007 <braydenpoveda@gmail.com>
2025-08-08 12:47:29 -0500

    feat: :sparkles: Jugadores (Terminado HASTA EL MOMENTO)

--- obj/Debug/net9.0/LigaTorneo.AssemblyInfo.cs
+++ obj/Debug/net9.0/LigaTorneo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LigaTorneo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c617db4c1745866c49be721534b9a6dcc612699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03e3034fd45ecb9e5d481623b60d23c0fd91971")]
 [assembly: System.Reflection.AssemblyProductAttribute("LigaTorneo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LigaTorneo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
